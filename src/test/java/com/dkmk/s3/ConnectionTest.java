293c0a13ebdd220276f68123e2bcac9f2ef1bdb5
Euuu127 <mak12cht7@gmail.com>
2021-03-22 16:41:51 +0900

    441

---
+++ src/test/java/com/dkmk/s3/ConnectionTest.java
@@ -0,0 +1,22 @@
+package com.dkmk.s3;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ConnectionTest extends MyAbstractTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void connectionTest() throws Exception {
+		assertNotNull(dataSource.getConnection());
+	}
+	
+
+}
