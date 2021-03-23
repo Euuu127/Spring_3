4cdc7bb25aedc046cced3bf44352da49ca371067
Euuu127 <mak12cht7@gmail.com>
2021-03-23 14:39:41 +0900

    Create MemberDAOTest.java

---
+++ src/test/java/com/dkmk/s3/member/MemberDAOTest.java
@@ -0,0 +1,14 @@
+package com.dkmk.s3.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dkmk.s3.MyAbstractTest;
+
+public class MemberDAOTest extends MyAbstractTest{
+	@Autowired
+	private MemberDAO memberDAO;
+
+}
