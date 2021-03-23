e286bccedfa690fd085d25d641326e1b1e66c3d2
Euuu127 <mak12cht7@gmail.com>
2021-03-23 10:27:58 +0900

    Update BankBookDAOTest.java

--- src/test/java/com/dkmk/s3/bankbook/BankBookDAOTest.java
+++ src/test/java/com/dkmk/s3/bankbook/BankBookDAOTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dkmk.s3.MyAbstractTest;
-							//테스트가 돌아가게 하기 위해 만들어둔 클래스 상속(?
+							
 public class BankBookDAOTest extends MyAbstractTest {
 	
 	@Autowired //주입시켜주쉐이~
