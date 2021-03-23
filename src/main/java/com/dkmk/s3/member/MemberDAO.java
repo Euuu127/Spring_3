68cbe1eacbb40dc208421ac70b213d08ff38c146
Euuu127 <mak12cht7@gmail.com>
2021-03-23 14:32:54 +0900

    memberlogin

--- src/main/java/com/dkmk/s3/member/MemberDAO.java
+++ src/main/java/com/dkmk/s3/member/MemberDAO.java
@@ -5,13 +5,21 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.xml.stream.events.Namespace;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import oracle.jdbc.driver.DBConversion;
 
 @Repository 
 public class MemberDAO {
+	@Autowired 						
+	private SqlSession sqlSession;
 	
+	private final String NAMESPACE= "com.dkmk.s3.bankbook.MemberDAO";
+			
 	public int memberUpdate(MemberDTO memberDTO)throws Exception{
 		//id를 제외하고 나머지 수정
 	}
@@ -22,17 +30,7 @@ public class MemberDAO {
 	
 	//memberJoin 데이터를 받아서 DB에 insert 하는 메서드
 	public int memberJoin(MemberDTO memberDTO)throws Exception{
-		//1. 로그인 정보 
-		String user="mark02";
-		String password="mark127";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		
-		//2. 클래스 로딩
-		Class.forName(driver);
-		
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
+
 		
 		//4. SQL
 		String sql ="insert into member values(?,?,?,?,?)";
@@ -60,47 +58,12 @@ public class MemberDAO {
 	
 	
 	//login - id pw를 받아서 조회
-	public MemberDTO memberLogin(MemberDTO memberDTO)throws Exception{
+	public int memberLogin(MemberDTO memberDTO)throws Exception{
 		
-		//1. 로그인 정보 
-		String user="mark02";
-		String password="mark127";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
+		return sqlSession.update(NAMESPACE+".memberLogin", memberDTO);
 		
-		//2. 클래스 로딩
-		Class.forName(driver);
 		
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
 		
-		//4. SQL문 생성
-		String sql ="SELECT * FROM member WHERE id=? and pw=?";
-		
-		//5. 미리 보내기
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		//6. ? 세팅
-		st.setString(1, memberDTO.getId());
-		st.setString(2, memberDTO.getPw());
-		
-		//7. 최종 전송 후 처리
-		ResultSet rs = st.executeQuery();
-		
-		if(rs.next()) {
-			memberDTO.setName(rs.getString("name"));
-			memberDTO.setEmail(rs.getString("email"));
-			memberDTO.setPhone(rs.getString("phone"));
-		}else {
-			memberDTO = null;
-		}
-		
-		//8. 해제
-		rs.close();
-		st.close();
-		con.close();
-		
-		return memberDTO;
 	}
 
 }
