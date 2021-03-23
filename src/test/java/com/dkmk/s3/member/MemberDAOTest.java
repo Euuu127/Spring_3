640a9015e4677c4cad0453b4f1df3d5e8a6e7b2e
Euuu127 <mak12cht7@gmail.com>
2021-03-23 15:06:48 +0900

    ´ . .̫ . ` ..

--- src/test/java/com/dkmk/s3/member/MemberDAOTest.java
+++ src/test/java/com/dkmk/s3/member/MemberDAOTest.java
@@ -11,8 +11,8 @@ public class MemberDAOTest extends MyAbstractTest{
 	@Autowired
 	private MemberDAO memberDAO;
 	
-	@Test
-	public void setWriteTest() throws Exception {
+	@Test //쪼인, 완, •̀.̫•́✧
+	public void memberJoinTest() throws Exception {
 		MemberDTO memberDTO = new MemberDTO();
 		memberDTO.setId("bam");
 		memberDTO.setPw("bam1");
@@ -21,6 +21,18 @@ public class MemberDAOTest extends MyAbstractTest{
 		memberDTO.setEmail("bk@naver.com");
 		int result = memberDAO.memberJoin(memberDTO);
 		
+		assertEquals(1, result);
+	}
+	
+	public void memberUpdateTest() throws Exception{
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId("bam");
+
+		memberDTO = memberDAO.memberLogin(memberDTO);
+		
+		memberDTO.setId("BUMgyu");
+	
+		int result = memberDAO.memberUpdate(memberDTO);
 		assertEquals(1, result);
 	}
 
