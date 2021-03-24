a62af1159d5ff359fbff804f18bb81a8c7fca64a
Euuu127 <mak12cht7@gmail.com>
2021-03-24 16:19:40 +0900

    Update MemberService.java

--- src/main/java/com/dkmk/s3/member/MemberService.java
+++ src/main/java/com/dkmk/s3/member/MemberService.java
@@ -1,13 +1,19 @@
 package com.dkmk.s3.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.dkmk.s3.member.MemberDAO;
 
+@Service
 public class MemberService {
 	
 	@Autowired
 	private MemberDAO memberDAO;
+	
+	public MemberDTO memberLogin(MemberDTO memberDTO)throws Exception{
+		return memberDAO.memberLogin(memberDTO);
+	}
 	
 	public int memberJoin(MemberDTO memberDTO)throws Exception{
 		return memberDAO.memberJoin(memberDTO);
