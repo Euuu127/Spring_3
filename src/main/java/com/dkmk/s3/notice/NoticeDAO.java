c652e3ee35fff41765ff992f2c20127460297603
Euuu127 <mak12cht7@gmail.com>
2021-03-28 20:57:27 +0900

    getlist mapper/dao

--- src/main/java/com/dkmk/s3/notice/NoticeDAO.java
+++ src/main/java/com/dkmk/s3/notice/NoticeDAO.java
@@ -14,8 +14,8 @@ public class NoticeDAO {
 	private final String NAMESPACE = "com.dkmk.s3.notice.NoticeDAO";
 	
 	//글리스트
-	public List<NoticeDTO> getList()throws Exception {
-		return sqlSession.selectList(NAMESPACE+"getList");
+	public List<NoticeDTO> getList()throws Exception {		//여기 ()안에 noticedto 받아와야되나?
+		return sqlSession.selectList(NAMESPACE+"getList"); //, noticedto 해야되나 ... 일단 없이 ㄱ
 	}
 	
 	
