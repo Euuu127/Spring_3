3f71ee10e94eb921d08377bce039811d667004ad
Euuu127 <mak12cht7@gmail.com>
2021-03-28 20:52:50 +0900

    /

--- src/main/java/com/dkmk/s3/notice/NoticeDAO.java
+++ src/main/java/com/dkmk/s3/notice/NoticeDAO.java
@@ -1,5 +1,24 @@
 package com.dkmk.s3.notice;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class NoticeDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE = "com.dkmk.s3.notice.NoticeDAO";
+	
+	//글리스트
+	public List<NoticeDTO> getList()throws Exception {
+		return sqlSession.selectList(NAMESPACE+"getList");
+	}
+	
+	
+	
 
 }
