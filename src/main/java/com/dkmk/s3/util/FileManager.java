a8d37ade1abc3144984fd315288d3489e39225b9
Euuu127 <mak12cht7@gmail.com>
2021-04-07 15:53:11 +0900

    fileManager

--- src/main/java/com/dkmk/s3/util/FileManager.java
+++ src/main/java/com/dkmk/s3/util/FileManager.java
@@ -1,26 +1,54 @@
 package com.dkmk.s3.util;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileManager {
 	
-	public void makePath(HttpSession session)throws Exception {
+	public void saveUseTransfer(MultipartFile multipartFile, File file)throws Exception{
+		multipartFile.transferTo(file);
+	}
+	
+	
+	public void saveUseCopy(MultipartFile multipartFile, File file)throws Exception{
+		FileCopyUtils.copy(multipartFile.getBytes(), file); //(byte배열,파일객체이름)
+	}
+	
+	
+	public String makeUUIDFileName()throws Exception{
+		return UUID.randomUUID().toString();
+	}
+	
+
+	public String makeTimeFileName()throws Exception{
+		Calendar ca = Calendar.getInstance();
+		long time = ca.getTimeInMillis();
+		return String.valueOf(time);
+	}
+
+
+	public File makePath(HttpSession session)throws Exception {
 
 		//	1. 저장할 폴더 지정 resources/upload/member
 		//	2. os에서 인식할 수 있는 실제 경로 찾기
 		//	jsp : application - server : ServletContext 	
-			String path = session.getServletContext().getRealPath("resources/upload/member");
-			System.out.println(path);
-			
-		//	3. 실제 경로에 폴더가 없으면 파일 저장시 에러 발생
-			File file = new File(path); //폴더의 경로를 담고 있는 객체 = file
+		String path = session.getServletContext().getRealPath("resources/upload/member");
+		System.out.println(path);
 
-			if(!file.exists()) {
-				file.mkdirs();
-			}		
-		
+		//	3. 실제 경로에 폴더가 없으면 파일 저장시 에러 발생
+		File file = new File(path); //폴더의 경로를 담고 있는 객체 = file
+
+		if(!file.exists()) {
+			file.mkdirs();
+		}		
+
+		return file;
 	}
 
 }
