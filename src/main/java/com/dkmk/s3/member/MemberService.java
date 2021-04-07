4621602ace80fa1b36a1314926c2f25c917a04af
Euuu127 <mak12cht7@gmail.com>
2021-04-07 16:15:35 +0900

    fileupload

--- src/main/java/com/dkmk/s3/member/MemberService.java
+++ src/main/java/com/dkmk/s3/member/MemberService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.dkmk.s3.util.FileManager;
+
 
 
 @Service
@@ -18,6 +20,8 @@ public class MemberService {
 	
 	@Autowired
 	private MemberDAO memberDAO;
+	@Autowired //주입~
+	private FileManager fileManager;
 	
 	public int memberUpdate(MemberDTO memberDTO)throws Exception{
 		return memberDAO.memberUpdate(memberDTO);
@@ -33,14 +37,8 @@ public class MemberService {
 												//파일에 대한 정보 avator에 있으
 							//문자				2진데이터
 	public int memberJoin(MemberDTO memberDTO, MultipartFile avator, HttpSession session)throws Exception{
-		//avator.getBytes(); 여기에 2진데이터 들어있는 거임
+		fileManager.save("member", avator, session);
 
-	//1~5은 FileManager.java에 있으
-		
-
-		
-		
-		
 		return 0;
 		// return memberDAO.memberJoin(memberDTO); 
 	}
--- src/main/java/com/dkmk/s3/util/FileManager.java
+++ src/main/java/com/dkmk/s3/util/FileManager.java
@@ -6,10 +6,42 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
 public class FileManager {
+	
+	public void save(String name, MultipartFile multipartFile, HttpSession session)throws Exception{
+		//1. 경로 설정 //avator.getBytes(); 여기에 2진데이터 들어있는 거임
+		String path = session.getServletContext().getRealPath("resources/upload/"+name);
+		System.out.println(path);
+		
+		File file = new File(path); //폴더의 경로를 담고 있는 객체 = file
+
+		if(!file.exists()) {
+			file.mkdirs();
+		}		
+		
+		//2. 저장할 파일명
+		String fileName="";
+		
+//		Calendar ca = Calendar.getInstance();
+//		long time = ca.getTimeInMillis();
+//		fileName = time+"_"+multipartFile.getOriginalFilename();
+		
+		fileName = UUID.randomUUID().toString()+"_"+multipartFile.getOriginalFilename();
+		
+		//3. HDD에 저장
+		file = new File(file, fileName);
+//		FileCopyUtils.copy(multipartFile.getBytes(), file);
+		multipartFile.transferTo(file);
+		
+		
+		
+	
+	}
 	
 	public void saveUseTransfer(MultipartFile multipartFile, File file)throws Exception{
 		multipartFile.transferTo(file);
