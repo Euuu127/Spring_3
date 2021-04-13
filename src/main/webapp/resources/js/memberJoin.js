bd5b8e27d8a9b16526eb9c97b4bb61759ed58d80
Euuu127 <mak12cht7@gmail.com>
2021-04-14 03:42:42 +0900

    /

--- src/main/webapp/resources/js/memberJoin.js
+++ src/main/webapp/resources/js/memberJoin.js
@@ -14,75 +14,86 @@ let pwEqualResult = false; //pw 같은지 체크 결과
 let etcResult = true; 	  //name, email, phone 체크
 
 
+// PW EQUAL CHECK **********************************
+pw2.addEventListener("blur", function(){
+	if(pw.value != pw2.value){
+		pw2.value="";
+	}else {
+		pwEqualResult=true;
+	}
+});
+
+pw.addEventListener("change", function(){
+	pw2.value="";
+});
+
+// **** PW CHECK *********************************
+
+pw.addEventListener("blur", function(){
+	pwCheckResult=false;
+	let message = "8글자 미만입니다";
+	let c = "r1";
+	if(pw.value.length>7){
+		message = "8글자 이상입니다"
+		c= "r2";
+		pwCheckResult=true;
+	}
+	
+	let pwResult = document.getElementById("pwResult");
+	pwResult.innerHTML=message;
+	pwResult.setAttribute("class", c);
+	
+});
+
+// ID Check *********************************
+id.addEventListener("blur", function(){
+	let message = "6글자 미만입니다";
+	let c = "r1"
+	if(id.value.length>5){
+		message = "6글자 이상 입니다";
+		c = "r2";
+		idCheckResult=true;
+	}else {
+		idCheckResult=false;
+	}
+	
+	let idResult = document.getElementById("idResult");
+	idResult.innerHTML=message;
+	idResult.setAttribute("class", c);
+	
+});
+// ***************************************************
+
 btn.addEventListener("click", function(){
 	for(let e of etc){
-		if(e.value =="") {
-			etcResult = false;
+		if(e.value == ""){
+			etcResult=false;
 			break;
 		}
 	}
 	
-	//조건이 만족하면 서버로 보내고 /url주소로파라미터보내야.. = submit이벤트 강제발생
-	if(idCheckResult && pwCheckResult && pwEqualResult && etcResult) {
+	//조건이 만족하면
+	if(idCheckResult && pwCheckResult && pwEqualResult && etcResult){
 		let frm = document.getElementById("frm");
-		frm.submit(); //강제실행
-	} else {
-		alert("필수항목을 입력하세요")
+		//frm.submit();
+		alert("회원가입 진행");
+	}else {
+		alert("필수 항목을 입력하세요");
 	}
-	
 });
 
-//*************  ID 체크  ************ */
-/*id.addEventListener("blur", function(){
-	let dm = "6글자 미만입니다";
-	let c = "r1"
-	if(id.value.length>5){
-		dm = "6글자 이상입니다";
-		c="r2";
-		idCheckResult = true;
-		
-	}else{id.value="";}
-	let idResult = document.getElementById("idResult"); //쓰고 버릴거라 여기서 선언
-	idResult.innerHTML=dm;
-	idResult.setAttribute("class",c);
-	
-});*/
-
-//*************  ID 중복확인  ************ */
+// Id 중복 확인
 $("#id").blur(function(){
 	let id = $("#id").val();
 	$.get("./memberIdCheck?id="+id, function(result){
-		alert(result=='0');
-	});
-});
-
-//*************  PW 체크  ************ */
-pw.addEventListener("blur", function(){
-	let dm = "8글자 미만입니다";
-	let c = "r1"
-	if(pw.value.length>7){
-		dm = "8글자 이상입니다";
-		c="r2";
-		pwCheckResult = true;
-	}
-	let pwResult = document.getElementById("pwResult");
-	pwResult.innerHTML=dm;
-	pwResult.setAttribute("class", c);
-});
-
-//***********  PW 체인지  ********** */
-pw.addEventListener("change",function(){
-	alert('pw change');
-	if(pw.value != pw2.value){
-		pw2.value="";
+		result = result.trim();
+		let str ="사용가능한 ID 입니다";
+		
+		if(result=='0'){
+			str ="중복 ID 입니다";
 		}
-});
-
-pw2.addEventListener("blur", function(){
-	if(pw.value != pw2.value){
-		pw2.value="";
-	} else{
-		pwEqualResult=true;
-	}
-	
+		
+		$("#idCheckResult").html(str);
+		
+	});
 });
